1421d10fdae5ba9c1a7cb28bc48262937380dfed
Buse Uslu <buseuslu24@gmail.com>
2022-11-30 23:42:36 +0000

    implemented firebase librarrry

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,56 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyB4Di0H1x7xTWeXd5anqwAAtFo8ZhOGcfA',
+  authDomain: 'comfort-zone-db.firebaseapp.com',
+  projectId: 'comfort-zone-db',
+  storageBucket: 'comfort-zone-db.appspot.com',
+  messagingSenderId: '397174815703',
+  appId: '1:397174815703:web:d235215c321aed34f66992',
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: 'select_account',
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists());
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log('error creating the user', error.message);
+    }
+  }
+
+  return userDocRef;
+};
