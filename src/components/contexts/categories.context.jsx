153c24633a9bbc95c5fb46d7f0a2c51e67bf3473
Buse Uslu <buseuslu24@gmail.com>
2022-12-20 13:09:04 +0000

    finished category component nested routing

--- src/components/contexts/categories.context.jsx
+++ src/components/contexts/categories.context.jsx
@@ -12,7 +12,6 @@ export const CategoriesProvider = ({ children }) => {
   useEffect(() => {
     const getCatergoriesMap = async () => {
       const categoryMap = await getCategoriesAndDocuments();
-      console.log(categoryMap);
       setCategoriesMap(categoryMap);
     };
     getCatergoriesMap();
