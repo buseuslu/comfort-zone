7363eff842a45b136057d8208a6d49d7bf235463
Buse Uslu <buseuslu24@gmail.com>
2022-12-21 23:28:44 +0000

    fixed styling and made titles clickable to navigate

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,11 +1,15 @@
-import './category-preview.styles.scss';
+import { Link } from 'react-router-dom';
 import ProductCard from '../product-card/product-card.component';
+
+import './category-preview.styles.scss';
 
 const CategoryPreview = ({ title, products }) => {
   return (
     <div className='category-preview-container'>
-      <h2 className='title'>
-        <span>{title.toUpperCase()}</span>
+      <h2>
+        <Link className='title' to={title}>
+          {title.toUpperCase()}
+        </Link>
       </h2>
       <div className='preview'>
         {products
