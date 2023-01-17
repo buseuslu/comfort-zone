1ab626189ea7d2286497125535c464a8c5a05016
Buse Uslu <buseuslu24@gmail.com>
2023-01-17 12:50:51 +0000

    added some responsiveness

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import Checkout from './routes/checkout/checkout.component';
 import { useEffect } from 'react';
 import { checkUserSession } from './store/user/user.action';
 
+import { GlobalStyle } from './global.styles';
+
 const App = () => {
   const dispatch = useDispatch();
 
@@ -17,14 +19,17 @@ const App = () => {
   }, []);
 
   return (
-    <Routes>
-      <Route path='/' element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path='shop/*' element={<Shop />} />
-        <Route path='auth' element={<Authentication />} />
-        <Route path='checkout' element={<Checkout />} />
-      </Route>
-    </Routes>
+    <div>
+      <GlobalStyle />
+      <Routes>
+        <Route path='/' element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path='shop/*' element={<Shop />} />
+          <Route path='auth' element={<Authentication />} />
+          <Route path='checkout' element={<Checkout />} />
+        </Route>
+      </Routes>
+    </div>
   );
 };
 
